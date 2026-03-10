f344d86bcac78b753795ed7e907c2672659786ba
Junhwan Park <semoking@naver.com>
2026-03-10 19:50:37 +0900

    fuxi 03-10
    Signed-off-by: Junhwan Park <semoking@naver.com>

--- script.js
+++ script.js
@@ -613,6 +613,7 @@ const secretFiles = [
     { title: "2026-03-05 복습", path: "secret/2026-03-05.md" },
     { title: "2026-03-06 복습", path: "secret/2026-03-06.md" },
     { title: "2026-03-09 복습", path: "secret/2026-03-09.md" },
+    { title: "2026-03-10 복습", path: "secret/2026-03-10.md" },
 ];
 
 let clickCount = 0;
